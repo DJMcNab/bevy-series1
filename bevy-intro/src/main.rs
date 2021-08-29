1e4f438a600c781e23675e08c9c78b5d391bdec8
Daniel McNab <sc19djm@leeds.ac.uk>
2021-08-29 21:08:41 +0100

    Create the initial intro demonstration

---
+++ bevy-intro/src/main.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::BISQUE .into()),
+        sprite: Sprite::new(Vec2::new(120.0, 30.0)),
+        ..Default::default()
+    });
+}
